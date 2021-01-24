603b104a6ebdee3c3e3fe207f63929721a8e19fd
steve <steve-li-1998@outlook.com>
2021-01-24 18:42:48 +0100

    修正服务器响应格式

--- ClientAnwendung/src/programmieraufgaben/Client.java
+++ ClientAnwendung/src/programmieraufgaben/Client.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 /**
  * Die Server-Klasse enthält alle Methoden zum Erstellen, Verwenden und Schließen des Servers.
- *
+ * <p>
  * Für die Lösung der Aufgabe müssen die Methoden connect, disconnect,
  * request und extract befüllt werden.
  * Es dürfen beliebig viele Methoden und Klassen erzeugt werden, solange
@@ -23,106 +23,104 @@ public class Client {
     private boolean standby = true;
     private PrintWriter out = null;
     private BufferedReader in = null;
-
-    public Client()
-    {
+    
+    public Client() {
         String buffer;
         Scanner input = new Scanner(System.in);
         System.out.println("Mit welchem Server wollen Sie sich verbinden?\n");
         System.out.print("IP-Adresse: ");
         IPAddress = input.nextLine();
-        IPAddress = IPAddress.replace("localhost","127.0.0.1");
-        if (!IPAddress.equals("127.0.0.1"))
-        {
+        IPAddress = IPAddress.replace("localhost", "127.0.0.1");
+        if (!IPAddress.equals("127.0.0.1")) {
             System.out.println("\nFalsche IP-Adresse! Aktuell ist nur die IPv4-Adresse 127.0.0.1 und die Eingabe localhost möglich.\n");
             standby = false;
         }
-        if (standby)
-        {
+        if (standby) {
             System.out.print("Port: ");
             buffer = input.nextLine();
             port = Integer.parseInt(buffer);
-            if (port != 2020)
-            {
+            if (port != 2020) {
                 System.out.println("\nKein korrekter Port! Aktuell ist nur Port 2020 möglich.\n");
                 standby = false;
             }
         }
-
+        
     }
-
+    
     /**
      * Hier werden die Verbindungsinformationen abgefragt und eine Verbindung eingerichtet.
      */
     public void connect() {
-        if (!standby)
-        {
+        if (!standby) {
             return;
         }
         clientSocket = new Socket();
-        try
-        {
-            clientSocket.connect(new InetSocketAddress(IPAddress,port));
-            out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8),true);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(),StandardCharsets.UTF_8));
-        } catch (IOException e)
-        {
+        try {
+            clientSocket.connect(new InetSocketAddress(IPAddress, port));
+            out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
+        } catch (IOException e) {
             System.out.println("Fehler beim Verbindungsaufbau! Es konnte keine TCP-Verbindung zum Server mit IP-Adresse localhost (Port: 2020) hergestellt werden.");
         }
-        System.out.println("Eine TCP-Verbindung zum Server mit IP-Adresse localhost (Port: 2020) wurde hergestellt. Sie können nun Ihre Anfragen an den Server stellen.");
+        System.out.println("\nEine TCP-Verbindung zum Server mit IP-Adresse localhost (Port: 2020) wurde hergestellt. Sie können nun Ihre Anfragen an den Server stellen.\n");
     }
-
+    
     /**
      * Hier soll die Verbindung und alle Streams geschlossen werden.
      */
     public void disconnect() {
         out.println("EXIT");
-        try
-        {
+        try {
             clientSocket.close();
             in.close();
             out.close();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("Fail to disconnect");
         }
         clientSocket = null;
         System.out.println("Die Verbindung zum Server wurde beendet.\n");
     }
-
+    
     /**
      * In dieser Methode sollen die Eingaben des Benutzers an den Server gesendet und die Antwort empfangen werden
+     *
      * @param userInput Eingabe des Benutzers
      * @return Die vom Server empfangene Nachricht
      */
     public String request(String userInput) {
         out.println(userInput);
-        try
-        {
+        try {
             return in.readLine();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("Fail to get response");
         }
         return null;
     }
-
+    
     /**
      * Die vom Server empfangene Nachricht soll hier für die Konsolenausgabe aufbereitet werden.
+     *
      * @param reply Die vom Server empfangene Nachricht
      * @return Ausgabe für die Konsole
      */
     public String extract(String reply) {
         StringBuilder res = new StringBuilder();
-        res.append(reply.replace("\\n","\n"));
+        String[] buffer;
+        buffer = reply.split("\\s+");
+        for (int i = 1; i < buffer.length; ++i) {
+            res.append(buffer[i]);
+            if (i != buffer.length - 1) {
+                res.append(" ");
+            }
+        }
         res.append("\n");
-        return res.toString();
+        reply = res.toString().replace("\\n", "\n");
+        return reply;
     }
-
+    
     /**
      * Gibt den Status der Verbindung an
+     *
      * @return Wenn die Verbindung aufgebaut ist: TRUE sonst FALSE
      */
     public boolean isConnected() {
--- ServerAnwendung/src/programmieraufgaben/Server.java
+++ ServerAnwendung/src/programmieraufgaben/Server.java
@@ -13,13 +13,13 @@ import java.util.List;
 
 /**
  * Die Server-Klasse enthält alle Methoden zum Erstellen, Verwenden und Schließen des Servers.
- *
+ * <p>
  * Für die Lösung der Aufgabe müssen die Methoden execute, disconnect
  * und checkPort befüllt werden.
  * Es dürfen beliebig viele Methoden und Klassen erzeugt werden, solange
  * die von den oben genannten Methoden aufgerufen werden.
  */
-public class Server{
+public class Server {
     private int port;
     private ServerSocket serverSocket;
     private PrintWriter out = null;
@@ -27,290 +27,306 @@ public class Server{
     private Socket client = null;
     private boolean continueExecute = true;
     private List<String> historyCommand = new LinkedList<>();
-
-
-    public Server()
-    {
-        try
-        {
+    
+    
+    public Server() {
+        try {
             serverSocket = new ServerSocket();
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("Feler! \"serverSocket\" kann nicht erzeugt werden!");
         }
     }
-
+    
     /**
      * Diese Methode beinhaltet die gesamte Ausführung (Verbindungsaufbau und Beantwortung
      * der Client-Anfragen) des Servers.
      */
     public void execute() {
-        while (continueExecute)
-        {
+        while (continueExecute) {
             String buffer = null;
-            try
-            {
+            try {
                 client = serverSocket.accept();
-                out = new PrintWriter(new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8),true);
-                in = new BufferedReader(new InputStreamReader(client.getInputStream(),StandardCharsets.UTF_8));
-                try
-                {
-                    while (!client.isClosed())
-                    {
+                out = new PrintWriter(new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
+                try {
+                    while (!client.isClosed()) {
                         buffer = in.readLine();
                         response(buffer);
                     }
-                }
-                catch (SocketException e)
-                {
+                } catch (SocketException e) {
                     System.out.println("Connection reset");
                 }
-            } catch (IOException e)
-            {
-                System.out.println("Die Verbindung kann nicht aufgebaut werden");
+            } catch (IOException e) {
+            
             }
-            if (null != client)
-            {
+            if (null != client) {
                 closeClient();
             }
         }
-
+        
     }
-
+    
     /**
      * Hier wird der Server die Kommando bearbeiten
+     *
      * @param command ist die Kommando (als einzel String) von Client
      */
-    private void response(String command)
-    {
-        if (command != null)
-        {
+    private void response(String command) {
+        if (command != null) {
             command = command.trim();
             String[] cmd = null;
             cmd = command.split("\\s+", 4);
-            if (1 == cmd.length & cmd[0].equals("EXIT")){
+            if (1 == cmd.length & cmd[0].equals("EXIT")) {
                 closeClient();
             }
-            if (2 == cmd.length & cmd[0].equals("GET"))     // Zeit und Datum kommando
-            {
-                Date date = new Date();
-                if (cmd[1].equals("Time"))
-                {
-                    historyCommand.add("GET Time");
-                    String strDateFormat = "HH:mm:ss";
-                    SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
-                    out.println(sdf.format(date));
+            switch (cmd[0]) {
+                // Zeit und Datum kommando
+                case "GET" -> cmdDate(cmd);
+                // Arithmetisches Kommando
+                case "ADD" -> cmdArith(cmd);
+                case "SUB" -> cmdArith(cmd);
+                case "MUL" -> cmdArith(cmd);
+                case "DIV" -> cmdArith(cmd);
+                // ECHO Kommando
+                case "ECHO" -> {
+                    cmd = command.split("\\s+", 2);
+                    out.println("ECHO " + cmd[1]);
+                    historyCommand.add("ECHO " + cmd[1]);
                 }
-                else if (cmd[1].equals("Date"))
-                {
-                    historyCommand.add("GET Date");
-                    String strDateFormat = "dd.MM.yyyy";
-                    SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
-                    out.println(sdf.format(date));
-                }
-                else
-                {
-                    out.println("Unbekannte Anfrage!");
-                    // Unbekannte Anfrage!
-                }
-            }
-            // Arithmetisches Kommando
-            else if (3 == cmd.length & (cmd[0].equals("ADD") | cmd[0].equals("SUB") | cmd[0].equals("MUL") | cmd[0].equals("DIV")))
-            {
-                if (cmd[1].matches(".*\\D+.*") | cmd[2].matches(".*\\D+.*"))        //Falls Buchstaben enthält
-                {
-                    out.println("Falsches Format!");
-                }
-                else
-                {
-                    int var1 = Integer.valueOf(cmd[1]);
-                    int var2 = Integer.valueOf(cmd[2]);
-                    if (cmd[0].equals("ADD"))
-                    {
-                        int result = var1 + var2;
-                        out.println(result);
-                    }
-                    else if (cmd[0].equals("SUB"))
-                    {
-                        int result = var1 - var2;
-                        out.println(result);
-                    }
-                    else if (cmd[0].equals("MUL"))
-                    {
-                        int result = var1 * var2;
-                        out.println(result);
-                    }
-                    else if (cmd[0].equals("DIV"))
-                    {
-                        if (0 == var2)
-                        {
-                            out.println("undefined");
-                        }
-                        else
-                        {
-                            double result = (double) var1 / (double) var2;
-                            out.println(result);
-                        }
-                    }
-                    else
-                    {
-                        out.println("Unbekannte Anfrage!");
-                        // Unbekannte Anfrage!
-                    }
-                    historyCommand.add(cmd[0] + " " + cmd[1] + " " + cmd[2]);
-                }
-            }
-            else if (cmd[0].equals("ECHO"))
-            {
-                cmd = command.split("\\s+", 2);
-                out.println(cmd[1]);
-                historyCommand.add("ECHO " + cmd[1]);
-            }
-            else if (cmd[0].equals("DISCARD"))
-            {
-                out.println();
                 // Keine Antwort, da der Discard-Dienst alle empfangenen Daten verwirft.
-            }
-            else if (1 == cmd.length & cmd[0].equals("PING"))
-            {
-                out.println("PONG");
-                historyCommand.add("PING");
-            }
-            else if (cmd[0].equals("HISTORY") & cmd.length <= 2)
-            {
-                if (1 == cmd.length)        // Alle Historie bisher
-                {
-                    sendHistory();
-                    historyCommand.add(cmd[0]);
-                }
-                else        // letzte <Integer> Historie
-                {
-                    if (cmd[1].matches(".*\\D+.*"))      // Der Parameter der hietorie soll einen Integer sein
-                    {
-                        out.println("Falsches Format!");
-                    }
-                    else
-                    {
-                        int counter = Integer.valueOf(cmd[1]);
-                        sendHistory(counter);
-                        historyCommand.add(cmd[0] + " " + cmd[1]);
-                    }
-                }
-            }
-            else
-            {
-                out.println("Unbekannte Anfrage!");
-                // Unbekannte Anfrage!
+                case "DISCARD" -> out.println();
+                // PING Kommando
+                case "PING" -> cmdPing(cmd);
+                // HISTORIE Kommando
+                case "HISTORY" -> cmdHistory(cmd);
+                // Sonst: Unbekannte Anfrage!
+                default -> out.println("ERROR Unbekannte Anfrage!");
             }
         }
     }
-
+    
     /**
      * Hier wird die Verbindung von Client getrennt, und Historien gelöscht
      */
-    private void closeClient()
-    {
-        try
-        {
-            client.close();
-            historyCommand.clear();		// Die Historien löschen, nur nach der Trennung der Verbindung
-        }
-        catch (IOException e)
-        {
+    private void closeClient() {
+        try {
+            if (null != client) {
+                client.close();
+            }
+            historyCommand.clear();        // Die Historien löschen, nur nach der Trennung der Verbindung
+        } catch (IOException e) {
             System.out.println("Die Verbindung ist fehlgeschlagen!");
             e.printStackTrace();
         }
     }
-
+    
+    private void cmdDate(String[] cmd) {
+        if (2 != cmd.length) {
+            out.println("ERROR Falsches Format!");
+            return;
+        }
+        Date date = new Date();
+        if (cmd[1].equals("Time")) {
+            historyCommand.add("GET Time");
+            String strDateFormat = "HH:mm:ss";
+            SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
+            out.println("TIME " + sdf.format(date));
+        }
+        else if (cmd[1].equals("Date")) {
+            historyCommand.add("GET Date");
+            String strDateFormat = "dd.MM.yyyy";
+            SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
+            out.println("DATE " + sdf.format(date));
+        }
+        else {
+            out.println("ERROR Unbekannte Anfrage!");
+            // Unbekannte Anfrage!
+        }
+    }
+    
+    private void cmdArith(String[] cmd) {
+        if (3 != cmd.length) {
+            out.println("ERROR Falsches Format!");
+            return;
+        }
+        
+        //Falls Buchstaben enthält
+        if (cmd[1].matches(".*\\D+.*") | cmd[2].matches(".*\\D+.*")) {
+            out.println("ERROR Falsches Format!");
+            return;
+        }
+        int var1, var2;
+        try {
+            var1 = Integer.parseInt(cmd[1]);
+            var2 = Integer.parseInt(cmd[2]);
+        } catch (NumberFormatException e) {
+            out.println("ARITH_WARNING Die Parametern muss Integern sein!");
+            historyCommand.add(cmd[0] + " " + cmd[1] + " " + cmd[2]);
+            return;
+        }
+        switch (cmd[0]) {
+            case "ADD": {
+                int result = var1 + var2;
+                out.println("SUM " + result);
+                break;
+            }
+            case "SUB": {
+                int result = var1 - var2;
+                out.println("DIFFERENCE " + result);
+                break;
+            }
+            case "MUL": {
+                int result = var1 * var2;
+                out.println("PRODUCT " + result);
+                break;
+            }
+            case "DIV":
+                if (0 == var2) {
+                    out.println("QUOTIENT undefined");
+                }
+                else {
+                    double result = (double) var1 / (double) var2;
+                    out.println("QUOTIENT " + result);
+                }
+                break;
+            default:
+                out.println("ERROR Unbekannte Anfrage!");
+                // Unbekannte Anfrage!
+                break;
+        }
+        historyCommand.add(cmd[0] + " " + cmd[1] + " " + cmd[2]);
+        
+    }
+    
+    private void cmdPing(String[] cmd) {
+        if (1 != cmd.length) {
+            out.println("ERROR Falsches Format!");
+            return;
+        }
+        out.println("PONG");
+        historyCommand.add("PING");
+    }
+    
+    private void cmdHistory(String[] cmd) {
+        if (cmd.length > 2) {
+            out.println("ERROR Falsches Format!");
+            return;
+        }
+        // Alle Historie
+        if (1 == cmd.length) {
+            sendHistory();
+        }
+        // letzte <Integer> Historie
+        else {
+            // Der Parameter der hietorie soll einen Integer sein
+            if (cmd[1].matches(".*\\D+.*")) {
+                out.println("ERROR Falsches Format!");
+            }
+            else {
+                int counter;
+                try {
+                    counter = Integer.parseInt(cmd[1]);
+                } catch (NumberFormatException e) {
+                    out.println("HISTORY_WARNING Die Parametern muss Integern sein!");
+                    historyCommand.add(cmd[0] + " " + cmd[1]);
+                    return;
+                }
+                sendHistory(counter);
+            }
+        }
+    }
+    
     /**
      * Hier soll die Historien gesendet werden
+     *
      * @param limit ist die Beschränkung der Anzahl der gesendet Historien, für limit = -1 gibt es kein Anzahl beschränkung
      */
-    private void sendHistory(int limit)
-    {
-        if (limit > historyCommand.size() | -1 == limit)
-        {
+    private void sendHistory(int limit) {
+        if (0 == historyCommand.size()) {
+            out.println("ERROR Keine Historie vorhanden!");
+            return;
+        }
+        int limit_copy = limit;
+        if (limit > historyCommand.size() | -1 == limit) {
             limit = historyCommand.size();
         }
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = historyCommand.size() - 1; i >= historyCommand.size() - limit; i--)
-        {
+        for (int i = historyCommand.size() - 1; i >= historyCommand.size() - limit; i--) {
             stringBuilder.append(historyCommand.get(i));
-            if (i != historyCommand.size() - limit)
-            {
+            if (i != historyCommand.size() - limit) {
                 stringBuilder.append("\\n");
             }
         }
-        out.println(stringBuilder.toString());
+        // "HISTORY <Sttring>" ist das Antwortformat
+        out.println("HISTORY " + stringBuilder.toString());
+        if (-1 == limit_copy) {
+            historyCommand.add("HISTORY");
+        }
+        else {
+            historyCommand.add("HISTORY " + limit);
+        }
     }
-
+    
     /**
      * Hier soll alle Historien gesendet
      */
-    private void sendHistory()
-    {
+    private void sendHistory() {
         sendHistory(-1);
     }
-
+    
     /**
      * Hier soll die Verbindung und alle Streams geschlossen werden.
      */
     public void disconnect() {
         continueExecute = false;
-        try
-        {
-            if (null != out)
-            {
+        try {
+            if (null != out) {
                 out.close();
             }
-            if (null != in)
-            {
+            if (null != in) {
                 in.close();
             }
-            if (null != client)
-            {
+            if (null != client) {
                 closeClient();
             }
-            if (null != serverSocket)
-            {
+            if (null != serverSocket) {
                 serverSocket.close();
             }
-
-
-        } catch (IOException e)
-        {
+            
+            
+        } catch (IOException e) {
             System.out.println("Feler! \"serverSocket\" kann nicht abgeschlossen werden!");
         }
+        System.out.println();
     }
-
+    
     /**
      * Überprüfung der Port-Nummer und Speicherung dieser in die Klassen-Variable "port"
+     *
      * @param port Portnummer als String
      * @return Port-Nummer ist akzeptabel TRUE oder nicht FALSE
      */
     public boolean checkPort(String port) {
-        if (!port.equals("2020"))
-        {
+        if (!port.equals("2020")) {
             System.out.println("Kein korrekter Port! Aktuell ist nur Port 2020 möglich.");
             return false;
         }
         this.port = 2020;
-        try
-        {
+        try {
             serverSocket.bind(new InetSocketAddress(this.port));
-        } catch (IOException e)
-        {
+        } catch (IOException e) {
             System.out.println("Fehler! \"serverSocket\" kann nicht mit Port gebunden werden!");
         }
         return true;
     }
-
+    
     /**
      * Gibt die akzeptierte und gespeicherte Port-Nummer zurück
-     * @return
+     *
+     * @return ist aktuelle Port
      */
     public int getPort() {
         return port;
     }
-
+    
 }
